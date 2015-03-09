513df6be825b9f666777c27c76b1550a1be9745c
snugthejoiner <joiner@jedparish.com>
2015-03-09 16:39:01 +0000

    Complete authentication continued assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :flash_attack
+  
+    def flash_attack
+        flash[:notice] = "Flash!"
+    end
+  
   protect_from_forgery with: :exception
    before_action :configure_permitted_parameters, if: :devise_controller?
  
@@ -7,4 +14,5 @@ class ApplicationController < ActionController::Base
    def configure_permitted_parameters
      devise_parameter_sanitizer.for(:sign_up) << :name
    end
+  
 end
