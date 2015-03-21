43d7aba2bcf8451235e0ce255ca617a8362b96ad
snugthejoiner <joiner@jedparish.com>
2015-03-21 02:50:19 +0000

    more fixes for summaries

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,28 +1,30 @@
 class SummariesController < ApplicationController
   def new
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:post_id])
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
     @summary = Summary.new
     authorize @summary
   end
   
   def show
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:post_id]).summary
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id]).summary
+    @summary = Summary.find(params[:id])
   end
   
   def create
-       @topic = Topic.find(params[:topic_id]) #find the topic id
-       @post = Post.find(params[:post_id]) #find the post id
-      @summary = Summary.new(params.require(:summary).permit(:name, :description)) #create/build the summary
-      @summary.post = @post
-      authorize @summary
-      if @summary.save
-        flash[:notice] = "Summary was saved."
-        redirect_to [@topic, @post, @summary] #go to the summary page
-      else
-        flash[:error] = "There was an error saving the summary. Please try again."
-        render :show #new makes no sense to me
-      end
+    @topic = Topic.find(params[:topic_id]) #find the topic id
+    @post = Post.find(params[:post_id]) #find the post id
+    @summary = Summary.new(params.require(:summary).permit(:name, :description)) #create/build the summary
+    @summary.post = @post
+    authorize @summary
+    
+    if @summary.save
+      flash[:notice] = "Summary was saved."
+      redirect_to [@topic, @post, @summary] #go to the summary page
+    else
+      flash[:error] = "There was an error saving the summary. Please try again."
+      render :show #new makes no sense to me
+    end
   end
 end
