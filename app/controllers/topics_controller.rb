29dbef10c284d883ea7e21d79ab6b6ab5ad504d8
snugthejoiner <joiner@jedparish.com>
2015-03-23 03:25:12 +0000

    Complete validating-posts assignment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -45,6 +45,6 @@ class TopicsController < ApplicationController
 
   private
     def topic_params
-      params.require(:topic).permit(:title, :body)
+      params.require(:topic).permit(:name, :description, :private)
     end
 end
