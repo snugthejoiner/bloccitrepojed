abd474aa82017acc6bab88918c00c3c1eda19b66
snugthejoiner <joiner@jedparish.com>
2015-03-27 23:54:00 +0000

    mailgun and post 2525

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "jed@jedparish.com"
+  layout 'mailer'
+end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,17 +1,3 @@
-#if Rails.env.development? || Rails.env.production?
-#  ActionMailer::Base.delivery_method = :smtp
-#  ActionMailer::Base.smtp_settings = {
-#    address:        'smtp.sendgrid.net',
-#    port:           '587',
-#    authentication: :plain,
-#    user_name:      ENV['SENDGRID_USERNAME'],
-#    password:       ENV['SENDGRID_PASSWORD'],
-#    domain:         'heroku.com',
-#    enable_starttls_auto: true,
-#    ssl: true
-#  }
-#end
-
 ActionMailer::Base.smtp_settings = {
   port:              2525, 
   address:           'smtp.mailgun.org',
