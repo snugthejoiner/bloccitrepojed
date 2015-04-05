de1dc74205a3c497360d90b5aba6046e42b2f8d1
snugthejoiner <joiner@jedparish.com>
2015-04-05 01:37:43 +0000

    deleted comment to test heroku suspicion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,6 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(params.require(:comment).permit(:body)) 
     @comment.post = @post
-#    authorize @comment
     if @comment.save
       flash[:notice] = "Comment was saved."
       redirect_to [@topic, @post]
