eb506efe41664a222d1aaf87b301cef48b440b9f
snugthejoiner <joiner@jedparish.com>
2015-04-16 21:06:46 +0000

    fixed comments_controll error redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to [@post]
+      redirect_to [@topic, @post]
     end
   end
     
