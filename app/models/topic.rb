b89905272382279b02650b9202e3252af7e51e0f
snugthejoiner <joiner@jedparish.com>
2015-04-23 23:46:27 +0000

    private-topics-assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,10 @@
 class Topic < ActiveRecord::Base
     has_many :posts, dependent: :destroy
     
+    scope :publicly_viewable, -> { where(public: true) }
+    scope :privately_viewable, -> { where(public: false) }
+    scope :visible_to, ->(user) { user ? all : where(public: true)}
+    
     validates :name, length: { minimum: 5 }, presence: true
     
-    scope :visible_to, -> (user) { user ? all : where(public: true) }
 end
--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -5,7 +5,7 @@
    end
    
    def show?
-   record.public? || user.present?
+     record.public? || user.present?
    end
  
    def create?
